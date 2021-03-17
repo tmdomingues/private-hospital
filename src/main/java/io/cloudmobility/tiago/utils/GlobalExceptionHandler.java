2016345a2fad5bd0ad947789ab49825ae63f61ea
Tiago Domingues <tiago.domingues@ibanwallet.com>
2021-03-17 02:23:03 +0000

    Code refactors
    Renaming and moving classes

--- src/main/java/io/cloudmobility/tiago/utils/GlobalExceptionHandler.java
+++ src/main/java/io/cloudmobility/tiago/utils/GlobalExceptionHandler.java
@@ -1,11 +1,12 @@
 package io.cloudmobility.tiago.utils;
 
-
 import java.time.format.DateTimeParseException;
+import java.util.NoSuchElementException;
 
 import org.springframework.core.convert.ConversionFailedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -28,23 +29,31 @@ class GlobalExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handleIllegalArgumentException(final RuntimeException ex) {
+        log.error(ex.getMessage());
         return new ResponseEntity(new JsonErrorPayload(ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(DateTimeParseException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handleDateTimeParseException(final RuntimeException ex) {
-        final var customMessage = ex.getMessage() + ". Use the expected pattern: 'yyyy-MM-dd HH:mm'";
+        final var customMessage = ex.getMessage() + ". Expected pattern: 'yyyy-MM-ddTHH:mm'";
         log.error(customMessage);
-        return new ResponseEntity(new JsonErrorPayload(ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity(new JsonErrorPayload(customMessage), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ResponseEntity<String> handleGeneralException(final RuntimeException ex) {
-        final String customMessage = "Error processing the request...If error persists please email us at stuff@medical.com or contact us at 555111999";
+    @ExceptionHandler(BadCredentialsException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ResponseEntity<String> handleBadCredentialsException(final RuntimeException ex) {
         log.error(ex.getMessage());
-        return new ResponseEntity(new JsonErrorPayload(customMessage), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity(new JsonErrorPayload(ex.getMessage()), HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleNoSuchElementException(final RuntimeException ex) {
+        final var customMessage = ex.getMessage() + ". No doctor/patient existing on the system with given id'";
+        log.error(customMessage);
+        return new ResponseEntity(new JsonErrorPayload(ex.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @Data
--- src/main/java/io/cloudmobility/tiago/utils/OpenApiConfig.java
+++ src/main/java/io/cloudmobility/tiago/utils/OpenApiConfig.java
@@ -1,12 +1,28 @@
 package io.cloudmobility.tiago.utils;
 
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "Private Hospital API", version = "v1"))
 public class OpenApiConfig {
 
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearer-jwt",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER).name("Authorization")))
+                .addSecurityItem(
+                        new SecurityRequirement().addList("bearer-jwt", Arrays.asList("read", "write")));
+    }
 }
